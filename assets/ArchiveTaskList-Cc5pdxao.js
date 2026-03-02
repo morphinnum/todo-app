fdb044e5f85b9fd45a0556ba203391d032339e8e
arina <aa.kremin@gmail.com>
2026-03-02 11:00:34 +0200

    Updates

---
+++ assets/ArchiveTaskList-Cc5pdxao.js
@@ -0,0 +1,1 @@
+import{j as t,T as r}from"./index-BMLmwp0R.js";function e({onEditTask:s}){return t.jsx(r,{isArchived:!0,onEditTask:s})}export{e as default};
--- assets/ArchiveTaskList-DzvCeodu.js
+++
@@ -1,1 +0,0 @@
-import{j as t,T as r}from"./index-RtRprfEy.js";function e({onEditTask:s}){return t.jsx(r,{isArchived:!0,onEditTask:s})}export{e as default};
